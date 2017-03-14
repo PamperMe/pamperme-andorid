415031b89eda8aa221afac3c20bb454ccd314b6b
valentin <valyo.mir@gmail.com>
2017-03-14 16:49:42 +0000

    edited navdraw

---
+++ app/src/main/java/pamperme/pamperme/PamperMe.java
@@ -0,0 +1,15 @@
+package pamperme.pamperme;
+
+import android.app.Application;
+
+/**
+ * Created by valentin on 14/03/2017.
+ * All rights reserved.
+ */
+
+public class PamperMe extends Application {
+
+    public PamperMe() {
+    }
+
+}
